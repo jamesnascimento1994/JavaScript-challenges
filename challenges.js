08c647140d37b60763769bc333deea5996ea6fa1
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-02-25 13:02:06 -0500

    addition function solution

---
+++ challenges.js
@@ -0,0 +1,12 @@
+/*
+Challenge One: Create a function that takes two numbers as arguments and return their sum
+Source: https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+*/
+
+// function addition(a, b) {
+//     return a + b;
+// }
+// // TESTS
+// console.log(addition(3, 5));
+// console.log(addition(-4, -2));
+// console.log(addition(56, 34));

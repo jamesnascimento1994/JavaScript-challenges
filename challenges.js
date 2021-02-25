13251e48da7a6aff5f3a401c0cab5dcf3c48465b
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-02-25 13:25:39 -0500

    addition increment function and bitwise operator function solutions

--- challenges.js
+++ challenges.js
@@ -1,6 +1,6 @@
 /*
 Challenge One: Create a function that takes two numbers as arguments and return their sum
-Source: https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+Challenge Source: https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
 */
 
 // function addition(a, b) {
@@ -10,3 +10,38 @@ Source: https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
 // console.log(addition(3, 5));
 // console.log(addition(-4, -2));
 // console.log(addition(56, 34));
+
+/* 
+Challenge Two: create a function that takes a number, increments it by +1, and returns the result
+Challenge Source: https://edabit.com/challenge/NAQhEoxbofPidLxm9
+*/
+
+// function addition(num) {
+//     num = num + 1;
+//     return num;
+// }
+
+// // TESTS
+// console.log(addition(32));
+// console.log(addition(6));
+// console.log(addition(454));
+
+/* 
+Challenge Three: Write 3 functions to calculate the bitwise AND, bitwise OR, and bitwise XOR of two numbers.
+Challenge Source: https://edabit.com/challenge/vvuAkYEAArrZvmp6X
+*/
+
+// function bitwiseAND(n1, n2) {
+//     return n1 & n2;
+// }
+// function bitwiseOR(n1, n2) {
+//     return n1 | n2;
+// }
+// function bitwiseXOR(n1, n2) {
+//     return n1 ^ n2;
+// }
+
+// // TESTS
+// console.log(bitwiseAND(7, 12));
+// console.log(bitwiseOR(7, 12));
+// console.log(bitwiseXOR(7, 12));

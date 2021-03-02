e497b8023d3ecf9651bb814f92dd871c2da6f6d9
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-01 21:51:15 -0500

    solution for morse code decoder function

--- challenges.js
+++ challenges.js
@@ -378,3 +378,43 @@ Challenge Source: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/javascr
 // console.log(duplicateCount('text'));
 // console.log(duplicateCount('indivisibility'));
 // console.log(duplicateCount('Woodpecker'));
+
+/* 
+Challenge Twenty-Three: In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
+Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+Challenge Source: https://www.codewars.com/kata/54b724efac3d5402db00065e/javascript
+*/
+
+// NOTE: MORSE_CODE on line 404 is exclusive to codewars. I'm not sure how to fix the error on VS code without having to create a giant array of every letter with its morse code.
+
+// decodeMorse = function (morseCode) {
+// 	// Split string of code into single words
+// 	// (ex: '.... --- .-- .- .-. . -.-- --- ..-' becomes '.... --- .--', '.- .-. .', '-.-- --- ..-')
+// 	morseCode = morseCode.split('  ');
+// 	let result = '';
+// 	// Iterate over each word and then split them into separate characters
+// 	for (let i = 0; i < morseCode.length; i++) {
+// 		morseCode[i] = morseCode[i].split(' ');
+// 		// create a nested loop inside that iterates over every morse character
+// 		// within the nested for loop
+// 		// create a conditional that translates the code into their equivalent alphabetical character
+// 		for (let a = 0; a < morseCode[i].length; a++) {
+// 			if (morseCode[i][a] != '') {
+// 				result += MORSE_CODE[morseCode[i][a]];
+// 			}
+// 		}
+// 		// create a conditional that adds a space after each word
+// 		if (i < morseCode.length - 1) {
+// 			result += ' ';
+// 		}
+// 	}
+// 	// finally, return the new string with the whitespaces removed
+// 	return result.trim();
+// };
+
+// // TESTS
+// console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
+// console.log(decodeMorse('-.-- -.-- --..'));

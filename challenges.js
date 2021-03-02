9f1028b0a2434267ac3958368b970ddd3aecbd8a
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-01 21:29:51 -0500

    solution fo dubstep song decoder

--- challenges.js
+++ challenges.js
@@ -338,3 +338,20 @@ Challenge Source: https://www.codewars.com/kata/54ff3102c1bad923760001f3/javascr
 // console.log(getCount("vowel"));
 // console.log(getCount('cover'));
 // console.log(getCount('cranium'));
+
+/* 
+Challenge Twenty-One: Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them. Let's assume that a song consists of some number of words (that don't contain WUB). To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club. For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed. Help Jonny restore the original song.
+Challenge Source: https://www.codewars.com/kata/551dc350bf4e526099000ae5/javascript
+*/
+
+// function songDecoder(song) {
+// 	// remove WUB from the string
+// 	// filter out the empty strings
+// 	// create spacing between words and join separate strings
+// 	return (song = song.split('WUB').filter(Boolean).join(' '));
+// }
+
+// // TESTS
+// console.log(songDecoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'));
+// console.log(songDecoder("WUBFORGIVEWUBMEWUBFATHERWUB"));
+// console.log(songDecoder("WUBJESSEWUBBADWUBBOYWUBJUSTWUBCOMEWUBLOOKWUBATWUBWHATWUBYOURWUBBROTHERWUBDIDWUB"));

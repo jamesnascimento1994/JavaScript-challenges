b76ee3f1c316810abb1505ba5f88edf3cc38cce9
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-04 17:50:30 -0500

    solution for disemvowel function

--- challenges.js
+++ challenges.js
@@ -438,3 +438,17 @@ Challenge Source: https://www.codewars.com/kata/50654ddff44f800200000004/javascr
 // console.log(multiply(5,4));
 // console.log(multiply(8,6));
 // console.log(multiply(9, 7));
+
+/* 
+Challenge Twenty-Five: Trolls are attacking your comment section! A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat. Your task is to write a function that takes a string and return a new string with all vowels removed. For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+Challenge Source: https://www.codewars.com/kata/52fba66badcd10859f00097e/javascript
+*/
+
+// function disemvowel(str) {
+//     return str.replace(/[aeiou]/gi, "");
+// }
+
+// // TESTS
+// console.log(disemvowel("This website is for losers LOL"));
+// console.log(disemvowel("Trolling on"));
+// console.log(disemvowel("Now are you for real?"));

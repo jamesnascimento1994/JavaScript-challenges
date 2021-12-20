f18cef5ae2cf6d3dba8972548d1d570681480620
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-12-20 14:23:39 -0500

    added solution for PaginationHelper challenge

--- challenges.js
+++ challenges.js
@@ -551,3 +551,66 @@ Challenge Source: https://www.codewars.com/kata/571d42206414b103dc0006a1/javascr
 // console.log(arr(5));
 // console.log(arr(7));
 // console.log(arr(8));
+
+/* 
+Challenge Thirty: For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array. The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+Challenge Source: https://www.codewars.com/kata/515bb423de843ea99400000a
+*/
+
+// function PaginationHelper(collection, itemsPerPage){
+//     this.items = collection;
+//     this.itemsPerPage = itemsPerPage;
+    
+//   }
+  
+//   // returns the number of items within the entire collection
+//   PaginationHelper.prototype.itemCount = function() {
+//     return this.items.length;
+//   }
+  
+//   // returns the number of pages
+//   PaginationHelper.prototype.pageCount = function() {
+//     return Math.ceil(this.itemCount() / this.itemsPerPage);
+//   }
+  
+//   // returns the number of items on the current page. page_index is zero based.
+//   // this method should return -1 for pageIndex values that are out of range
+//   PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+//     var itemsPerPage = this.itemsPerPage;
+//     var itemCount = this.itemCount();
+//     var pageCount = this.pageCount();
+    
+//     if (pageIndex >= pageCount || pageIndex < 0) {
+//       return -1;
+//     } else {
+//      return this.items.slice(pageIndex * itemsPerPage, itemCount).splice(0, itemsPerPage).length; 
+//     }
+//   }
+  
+//   // determines what page an item is on. Zero based indexes
+//   // this method should return -1 for itemIndex values that are out of range
+//   PaginationHelper.prototype.pageIndex = function(itemIndex) {
+//     if (this.itemCount() === 0 || itemIndex < 0 || itemIndex > this.itemCount()) {
+//       return -1;
+//     }
+//     if (itemIndex === 0 || itemIndex / this.itemsPerPage === 1) {
+//       return 0;
+//     }
+    
+//     return Math.floor((itemIndex / this.itemsPerPage))
+//   }
+
+//   // TESTS
+//   var helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
+//   console.log(helper.pageCount()); //should == 2
+//   console.log(helper.itemCount()); //should == 6
+//   console.log(helper.pageItemCount(0)); //should == 4
+//   console.log(helper.pageItemCount(1)); // last page - should == 2
+//   console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
+  
+//   // pageIndex takes an item index and returns the page that it belongs on
+//   console.log(helper.pageIndex(5)); //should == 1 (zero based index)
+//   console.log(helper.pageIndex(2)); //should == 0
+//   console.log(helper.pageIndex(20)); //should == -1
+//   console.log(helper.pageIndex(-10)); //should == -1  

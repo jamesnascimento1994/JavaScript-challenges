36e3f07e4fa91da4341b47c6b2a0c1ea870a9889
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-12-22 21:55:52 -0500

    greed is good solution

--- challenges.js
+++ challenges.js
@@ -623,28 +623,116 @@ Write a function cakes(), which takes the recipe (object) and the available ingr
 Challenge Source: https://www.codewars.com/kata/525c65e51bf619685c000059
 */
 
-function cakes(recipe, available) {
-    // TODO: insert code
-    let amtOfCakes;
-    // iterate over each ingredient property in the recipe
-    for (let i in recipe) {
-    // Create a condition that checks if the ingredient is present in the available object
-      if (available[i]) {
-        // If all the required ingredients are available, we figure out how many cakes are possible to make
-        const possibleCakes = Math.floor(available[i] / recipe[i] || 0)
-        // Create another condition from within that checks if the amtOfCakes is undefined or if the possible cakes variable is less than the amtOfCakes
-        if (!amtOfCakes || possibleCakes < amtOfCakes) {
-          // If this condition is met, the possibleCakes variable gets assigned to the amtOfCakes variable
-          amtOfCakes = possibleCakes;
-        }
+// function cakes(recipe, available) {
+//     // TODO: insert code
+//     let amtOfCakes;
+//     // iterate over each ingredient property in the recipe
+//     for (let i in recipe) {
+//     // Create a condition that checks if the ingredient is present in the available object
+//       if (available[i]) {
+//         // If all the required ingredients are available, we figure out how many cakes are possible to make
+//         const possibleCakes = Math.floor(available[i] / recipe[i] || 0)
+//         // Create another condition from within that checks if the amtOfCakes is undefined or if the possible cakes variable is less than the amtOfCakes
+//         if (!amtOfCakes || possibleCakes < amtOfCakes) {
+//           // If this condition is met, the possibleCakes variable gets assigned to the amtOfCakes variable
+//           amtOfCakes = possibleCakes;
+//         }
+//       } else {
+//         // if this condition is not met, return 0, because not all the required ingredients are available
+//         return 0;
+//       }
+//     }
+//     return amtOfCakes;
+//   }
+
+//   // TESTS
+//   console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
+//   console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}));
+
+/* 
+Challenge Thirty-Two: Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+
+ Three 1's => 1000 points
+ Three 6's =>  600 points
+ Three 5's =>  500 points
+ Three 4's =>  400 points
+ Three 3's =>  300 points
+ Three 2's =>  200 points
+ One   1   =>  100 points
+ One   5   =>   50 point
+A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+Example scoring
+
+ Throw       Score
+ ---------   ------------------
+ 5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
+ 1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
+ 2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
+In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
+
+Challenge Source: https://www.codewars.com/kata/5270d0d18625160ada0000e4
+
+*/
+
+function score( dice ) {
+    // Fill me in!
+    // Store the number of points in a varialble. Initial value = 0
+    let points = 0;
+    // Create an obj called counts with properties called 1,2,3,4,5,6. Init val = 0
+    let counts = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0
+    }
+    // Loop through the dice parameter
+    for (let i = 0; i < dice.length; i++) {
+      counts[dice[i]] += 1 
+    }
+    // Check for the conditions for each possible score
+      if (counts[6] / 3 >= 1) {
+        let newSix = counts[6] - counts[6]%3;
+        points += (newSix/3) * 600;
+      }
+    if (counts[5] > 0) {
+      if (counts[5] % 3 === 0) {
+        points += ((counts[5]/3) * 500);
+      } else if (counts[5] / 3 >= 1) {
+        let newFive = counts[5] - counts[5]%3;
+        points += ((newFive/3) * 500) + ((counts[5]%3) * 50);
       } else {
-        // if this condition is not met, return 0, because not all the required ingredients are available
-        return 0;
+        points += ((counts[5]%3) * 50)
       }
     }
-    return amtOfCakes;
+    if (counts[4] / 3 >= 1) {
+      let newFour = counts[4] - counts[4]%3;
+      points += (newFour/3) * 400;
+    }
+    if (counts[3] / 3 >= 1) {
+      let newThree = counts[3] - counts[3]%3;
+      points += (newThree/3) * 300;
+    }
+    if (counts[2] / 3 >= 1) {
+      let newTwo = counts[2] - counts[2]%3;
+      points += (newTwo/3) * 200;
+    }
+    if (counts[1] > 0) {
+      if (counts[1] % 3 === 0) {
+        points += ((counts[1]/3) * 1000);
+      } else if (counts[1] / 3 >= 1) {
+        let newOne = counts[1] - counts[1]%3;
+        points += ((newOne/3) * 1000) + ((counts[1]%3) * 100)
+      } else {
+        points += ((counts[1]%3) * 100);
+      }
+    }
+    return points;
   }
 
   // TESTS
-  console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
-  console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}));
+  console.log(score([2, 3, 4, 6, 2]));
+  console.log(score([4, 4, 4, 3, 3]));
+  console.log(score([2, 4, 4, 5, 4]));

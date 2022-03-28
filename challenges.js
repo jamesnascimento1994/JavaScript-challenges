7254c0e1afdbf289a24192830b8399db14c4098b
jamesnascimento1994 <james.nascimento94@yahoo.com>
2022-03-27 22:01:15 -0400

    added solution for hashtag generator

--- challenges.js
+++ challenges.js
@@ -856,13 +856,41 @@ Note: your implementation does not need to support the shorthand form of hexadec
 Challenge Source: https://www.codewars.com/kata/5282b48bb70058e4c4000fa7
 */
 
-function hexStringToRGB(hexString) {
-  let rgb = [];
-  for (let i = 1; i < hexString.length - 1; i+=2) {
-    rgb.push(parseInt(hexString.substring(i, i+2), 16));
-  }
-  return {r: rgb[0], g: rgb[1], b: rgb[2]};
+// function hexStringToRGB(hexString) {
+//   let rgb = [];
+//   for (let i = 1; i < hexString.length - 1; i+=2) {
+//     rgb.push(parseInt(hexString.substring(i, i+2), 16));
+//   }
+//   return {r: rgb[0], g: rgb[1], b: rgb[2]};
+// }
+
+// // TESTS
+// console.log(hexStringToRGB("#FF9933"));
+
+/* 
+Challenge Thirty-Eight: The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+
+Challenge Source: https://www.codewars.com/kata/52449b062fb80683ec000024
+*/
+
+function generateHashtag (str) {
+  if (str.trim() === "") return false;
+  let wordArr = str.split(' ')
+                    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                    .join('');
+  let hashTaggedStr = "#" + wordArr.trim();
+  return hashTaggedStr.length > 140 ? false : hashTaggedStr;
 }
 
 // TESTS
-console.log(hexStringToRGB("#FF9933"));
+console.log(generateHashtag(" Hello there thanks for trying my Kata"));
+console.log(generateHashtag("    Hello     World   "));
+console.log(generateHashtag(""));
